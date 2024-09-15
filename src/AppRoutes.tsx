53a1ac3df60cc939605349998ca8198787ee8128
George Ofogba <ofogbageorge3@gmail.com>
2024-09-15 09:42:10 +0200

    cart added

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,6 +6,7 @@ import UserProfilePage from './pages/UserProfilePage';
 import ProtectedRoutes from './auth/ProtectedRoutes';
 import ManageRestaurantPage from './pages/ManageRestaurantPage';
 import SearchPage from './pages/SearchPage';
+import DetailPage from './pages/DetailPage';
 
 const AppRoutes = () => {
   return (
@@ -19,11 +20,21 @@ const AppRoutes = () => {
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
       <Route
         path="/search/:city"
         element={
           <Layout showHero={false}>
             <SearchPage />
+          </Layout>
+        }
+      />
+
+      <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
           </Layout>
         }
       />
